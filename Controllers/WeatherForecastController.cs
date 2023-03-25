3b1098b8819778b4385afb673bbfcfd78da68f46
Gerardo Alberto Orozco Serrano <gerardorozco@hotmail.com>
2023-03-24 21:55:44 -0500

    Este es un commit de prueba

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace webapidi.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Ninguno"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
